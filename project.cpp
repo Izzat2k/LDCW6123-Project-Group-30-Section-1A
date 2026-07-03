463f57f6d67f85469b55313b48e3b584224db940
Aereal <abecosmos123@gmail.com>
2026-07-03 18:53:37 +0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int rideChoice;
+    double distance;
+    char Peakhour;
+    double Fare;
+    double RatePerKM;
+    double surcharge;
+    double subtotal;
+    double totalFare;
+}
